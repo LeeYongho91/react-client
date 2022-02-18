dfffeac1f67a903b17b35240bb4df7f75e201740
Essence <skyks4848@naver.com>
2022-02-18 19:55:58 +0900

    test

--- server/src/databases/index.ts
+++ server/src/databases/index.ts
@@ -3,8 +3,7 @@ import config from 'config';
 import { logger } from '@utils/logger';
 
 async function connect() {
-  let dbUri = config.get<string>('mongoURI');
-  if (config.get<string>('env') === 'production') dbUri = process.env.MONGO_URI;
+  const dbUri = config.get<string>('mongoURI');
   try {
     await mongoose.connect(dbUri);
     logger.info('✅ ====== MONGODB CONNECT ====== ✅');
